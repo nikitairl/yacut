20f858c09bc2c6c6d3b1a66daa1f9ab422002b8f
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-03-21 15:59:55 +0300

    Working build

---
+++ yacut/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
+
+
+class URLForm(FlaskForm):
+    original_link = URLField(
+        "Длинная ссылка",
+        validators=[DataRequired(message="Поле не может быть пустым")],
+    )
+    custom_id = StringField(
+        "Короткая ссылка",
+        validators=[Length(max = 5), Optional()]
+    )
+    submit = SubmitField("Создать")
---
+++ yacut/views.py
@@ -0,0 +1,43 @@
+from flask import flash, redirect, render_template, url_for
+
+from . import app, db
+from .forms import URLForm
+from .models import URLMap
+
+
+@app.route("/", methods=["POST", "GET"])
+def index_view() -> str:
+    form = URLForm()
+
+    if not form.validate_on_submit():
+        return render_template("index.html", form=form)
+
+    if not form.custom_id.data:
+        form.custom_id.data = URLMap.get_unique_short_id()
+
+    if not URLMap.available_short(form.custom_id.data):
+        flash(f'Имя {form.custom_id.data} уже занято!')
+        return render_template("index.html", form=form)
+
+    result_object = URLMap(
+        original=form.original_link.data,
+        short=form.custom_id.data,
+    )
+    db.session.add(result_object)
+    db.session.commit()
+    form.custom_id.data = None
+    return render_template(
+        "index.html",
+        form=form,
+        short_link=url_for(
+            "redirect_to_url_view",
+            short_id=result_object.short,
+            _external=True,
+        ),
+    )
+
+
+@app.route("/<string:short_id>", strict_slashes=False, methods=["GET"])
+def redirect_to_url_view(short_id):
+    url = URLMap.query.filter_by(short=short_id).first_or_404()
+    return redirect(url.original)
